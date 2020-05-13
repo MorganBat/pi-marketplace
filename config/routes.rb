3a95ce7c86a8ebf130c315d207a0b2fce0f85fc6
Morgan <morgan.batterham@gmail.com>
2020-05-13 15:36:32 +1000

    setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get "/", to: "users#index"
+  root to: 'users#index'
   resources :listings
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
