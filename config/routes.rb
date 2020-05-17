ca5acb917e96481710836e4761ab17324cae8d3d
Morgan <morgan.batterham@gmail.com>
2020-05-17 19:38:24 +1000

    removed default user routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get "/", to: "listings#index"
   root to: 'listings#index'
   resources :listings
-  resources :users
+  # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
