6876f0976b6b0d5751f2e2fbbb15285009541e6b
Morgan <morgan.batterham@gmail.com>
2020-05-19 08:57:14 +1000

    added payment success page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/", to: "listings#index"
   root to: 'listings#index'
   resources :listings
+  get "/payments/success", to: "payments#success"
   # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
