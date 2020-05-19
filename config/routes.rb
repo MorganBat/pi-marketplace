742e9cc765414ff77b655e74113419080e1a45e3
Morgan <morgan.batterham@gmail.com>
2020-05-19 10:03:42 +1000

    Added webhook to stripe payments

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'listings#index'
   resources :listings
   get "/payments/success", to: "payments#success"
+  post '/payments/webhook', to: 'payments#webhook'
   # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
